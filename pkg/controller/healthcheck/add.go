0d90202302a02c2214d2d8291c55860aa19d4bc4
Kristian-ZH <k.zhelyazkov@sap.com>
2022-08-30 15:26:57 +0300

    Add health

--- pkg/controller/healthcheck/add.go
+++ pkg/controller/healthcheck/add.go
@@ -7,9 +7,11 @@ package healthcheck
 import (
 	"time"
 
-	"github.com/Kristian-ZH/gardener-extension-logging/pkg/constants"
+	"github.com/gardener/gardener-extension-shoot-networking-filter/pkg/constants"
 	healthcheckconfig "github.com/gardener/gardener/extensions/pkg/apis/config"
 	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck"
+	"github.com/gardener/gardener/extensions/pkg/controller/healthcheck/general"
+	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -36,7 +38,12 @@ func RegisterHealthChecks(mgr manager.Manager, opts healthcheck.DefaultAddArgs) 
 		mgr,
 		opts,
 		nil,
-		[]healthcheck.ConditionTypeToHealthCheck{},
+		[]healthcheck.ConditionTypeToHealthCheck{
+			{
+				ConditionType: string(gardencorev1beta1.ShootSystemComponentsHealthy),
+				HealthCheck:   general.CheckManagedResource(constants.ManagedResourceNamesShoot),
+			},
+		},
 	)
 }
 
